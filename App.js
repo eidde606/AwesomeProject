20a5be843f7d818a026e25345748595b0a869f04
eidde606 <eidde_606@yahoo.com>
2024-02-01 01:59:53 -0500

    updated connection to backend

--- App.js
+++ App.js
@@ -17,39 +17,61 @@ export default function App() {
   // Function to fetch goals from the server
   const fetchGoals = async () => {
     try {
-      const response = await axios.get("http://localhost:5001/api/goals");
-      setLifeGoals(response.data);
+      const response = await axios.get(
+        "https://lifegoals-eb3650612eef.herokuapp.com/api/goals"
+      );
+      const goalsWithIds = response.data.map((goal) => ({
+        ...goal,
+        _id: goal._id.toString(),
+      }));
+      setLifeGoals(goalsWithIds);
     } catch (error) {
       console.error("Error fetching goals:", error);
     }
   };
 
-  function startAddGoalHandler(params) {
+  function startAddGoalHandler() {
     setModalIsVisible(true);
   }
 
-  function endAddGoalHandler(params) {
+  function endAddGoalHandler() {
     setModalIsVisible(false);
   }
 
   // Function to add a new goal
   const addGoalHandler = async (enteredGoalText) => {
     try {
-      const response = await axios.post("http://localhost:5001/api/goals", {
-        text: enteredGoalText,
-      });
-      setLifeGoals((currentLifeGoals) => [...currentLifeGoals, response.data]);
+      const response = await axios.post(
+        "https://lifegoals-eb3650612eef.herokuapp.com/api/goals",
+        {
+          text: enteredGoalText,
+        }
+      );
+      const newGoal = { ...response.data, _id: response.data._id.toString() };
+      setLifeGoals((currentLifeGoals) => [...currentLifeGoals, newGoal]);
       endAddGoalHandler();
     } catch (error) {
       console.error("Error adding goal:", error);
     }
   };
 
-  function deleteGoalHandler(id) {
-    setLifeGoals((currentLifeGoals) => {
-      return currentLifeGoals.filter((goal) => goal.id !== id);
-    });
-  }
+  // Function to delete a goal
+  const deleteGoalHandler = async (id) => {
+    console.log("Deleting goal with id:", id);
+    try {
+      await axios.delete(
+        `https://lifegoals-eb3650612eef.herokuapp.com/api/goals/${id}`
+      );
+      setLifeGoals((currentLifeGoals) =>
+        currentLifeGoals.filter((goal) => goal._id !== id)
+      );
+    } catch (error) {
+      console.error(
+        "Error deleting goal:",
+        error.response?.data || error.message
+      );
+    }
+  };
 
   return (
     <>
@@ -68,17 +90,14 @@ export default function App() {
         <View style={styles.goalsContainer}>
           <FlatList
             data={lifeGoals}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  text={itemData.item.text}
-                  id={itemData.item.id}
-                  onDeleteItem={deleteGoalHandler}
-                  key={itemData.item.id}
-                />
-              );
-            }}
-            keyExtractor={(item) => item.id}
+            renderItem={(itemData) => (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item._id} // Use '_id' instead of 'id'
+                onDeleteItem={deleteGoalHandler}
+              />
+            )}
+            keyExtractor={(item) => item._id.toString()} // Use '_id' instead of 'id'
             alwaysBounceVertical={false}
           />
         </View>
